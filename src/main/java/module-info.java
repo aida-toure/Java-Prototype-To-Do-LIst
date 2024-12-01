65b9f7cdbfb9407751cc90ca8a69dbb0679df0ab
aida <atg.aida.toure@gmail.com>
2024-12-01 19:19:43 +0100

    Update1 : add gson dependency

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module org.example.todolist {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.example.todolist to javafx.fxml;
+    exports org.example.todolist;
+}
